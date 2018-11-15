8b102010b8b78338c66caf924c8093ee7b4d012d
chenhongze <188295924@qq.com>
2018-11-15 22:08:43 +0800

    菜单栏样式更新

--- app/views/index/Index_layout.vue
+++ app/views/index/Index_layout.vue
@@ -8,23 +8,23 @@
                     </div>
                     <div class="layout-nav">
                         <MenuItem name="index">
-                            <!-- <Icon type="md-home" /> -->
+                            <Icon type="md-home" />
                             首页
                         </MenuItem>
                         <MenuItem name="buy">
-                            <!-- <Icon type="md-nutrition" /> -->
+                            <Icon type="ios-cart" />
                             买车
                         </MenuItem>
                         <MenuItem name="sale">
-                           <!-- <Icon type="md-pizza" /> -->
+                           <Icon type="md-key" />
                             卖车
                         </MenuItem>
                         <MenuItem name="user">
-                            <!-- <Icon type="logo-tux" /> -->
+                            <Icon type="ios-contact" />
                             用户
                         </MenuItem>
                         <MenuItem name="park">
-                            <!-- <Icon type="ios-paper"></Icon> -->
+                            <Icon type="ios-car" />
                             停车场
                         </MenuItem>
                     </div>
@@ -42,6 +42,27 @@
     }
 </script>
 
-<style scoped>
-
+<style scoped lang="less">
+    .layout{
+        border: 1px solid #d7dde4;
+        background: #f5f7f9;
+        position: relative;
+        border-radius: 4px;
+        overflow: hidden;
+    }
+    .layout-logo{
+        width: 100px;
+        height: 30px;
+        background: #5b6270;
+        border-radius: 3px;
+        float: left;
+        position: relative;
+        top: 15px;
+        left: 20px;
+    }
+    .layout-nav{
+        width: 480px;
+        margin: 0 auto;
+        margin-right: 20px;
+    }
 </style>
